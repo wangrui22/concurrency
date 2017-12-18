4da5e05a8bae0c76fd518dfe3ecf2591a4061ce6
wangrui22 <worry_wang@163.com>
2017-12-18 16:48:59 +0800

    add shared memory test

--- gpu/cuda-win/cuda-win/main.cu
+++ gpu/cuda-win/cuda-win/main.cu
@@ -1,8 +1,9 @@
 #include <stdio.h>
 extern int cuda_add(int argc, char*argv[]);
 int cuda_matrix_mul(int argc, char* argv[]);
+int cuda_matrix_mul_s(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) 
 {
-    cuda_matrix_mul(argc,  argv);
+    cuda_matrix_mul_s(argc,  argv);
 }
